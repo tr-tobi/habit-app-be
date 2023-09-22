19efa93cee4b0ede6982369fef2c4ba443a3b5b5
Tobi Thomas <tobi@tobis-air.home>
2023-09-22 20:07:56 +0100

    handles seeding for database

---
+++ db/seed/run-seed.ts
@@ -0,0 +1,26 @@
+var mongoose = require("mongoose");
+var usersSchema = require("../../models/index");
+var completionSchema = require("../../models/habit-completion");
+
+var Users = mongoose.model("Users", usersSchema);
+var Completion = mongoose.model(
+  "habit_completion",
+  completionSchema,
+  "habit_completion"
+);
+
+exports.insertUsers = async (usersData: any) => {
+  try {
+    await Users.insertMany(usersData);
+  } catch (error) {
+    console.error("Error inserting users:", error);
+  }
+};
+
+exports.insertCompletion = async (completionData: any) => {
+  try {
+    await Completion.insertMany(completionData);
+  } catch (error) {
+    console.error("Error inserting users:", error);
+  }
+};
